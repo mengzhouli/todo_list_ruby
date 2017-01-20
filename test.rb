d1a29abfb2f2e73cce67b8eebc897c38df235cfa
Mengzhou <mengzhou.li@gmail.com>
2017-01-20 18:27:17 -0500

    update function

--- test.rb
+++ test.rb
@@ -4,8 +4,9 @@ module Menu
 		1) add
 		2) show
 		3) delete
-		4) write to a file
-		5) read from a file
+		4) update
+		5) write to a file
+		6) read from a file
 		Q) quit"
 	end
 
@@ -40,7 +41,17 @@ class List #list class
 	end
 
 	def delete(task_number)
+		if task_number <1 || task_number > all_tasks.length
+			raise "No such task number."
+		end
 		all_tasks.delete_at(task_number - 1)
+	end
+
+	def update(task_number, task)
+		if task_number <1 || task_number > all_tasks.length
+			raise "No such task number"
+		end
+		all_tasks[task_number -1] = task
 	end
 
 	def write_to_file(filename)
@@ -79,11 +90,22 @@ if __FILE__ == $PROGRAM_NAME
 		when "2"
 			my_list.show
 		when "3"
-			my_list.show
-			my_list.delete(prompt("What do you want to delete?").to_i)
+			begin
+				my_list.show
+				my_list.delete(prompt("What do you want to delete?").to_i)
+			rescue
+				puts "that task index does not exist!" 
+			end
 		when "4"
-			my_list.write_to_file(prompt("What is the filename to write to?"))
+			begin 
+				my_list.show
+				my_list.update(prompt("What do you want to update?").to_i, Task.new(prompt("What is the new task?")))
+			rescue
+				puts "that task index does not exist!"
+			end
 		when "5"
+			my_list.write_to_file(prompt("What is the filename to write to?"))
+		when "6"
 			begin
 				my_list.read_from_file(prompt("What is the filename to read from?"))
 			rescue Errno::ENOENT
